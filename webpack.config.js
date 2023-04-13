b03bdfe93b4f3ece6e1cff72223389a40c66bc82
Reece Vela <reecevela@outlook.com>
2023-04-12 23:31:45 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
+  mode: 'development',
   devtool: "source-map",
   module: {
     rules: [
